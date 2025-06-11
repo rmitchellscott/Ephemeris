f509de982754c2e5cd4a8bbe31c1b4dcc73dff38
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2025-06-11 09:42:56 -0600

    deps: move from pypdf2 to pypdf to address GHSA-4vvm-4w3v-6mr8

--- ephemeris.py
+++ ephemeris.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 from collections import Counter, defaultdict
 
-from PyPDF2 import PdfMerger
+from pypdf import PdfMerger
 from loguru import logger
 from reportlab.pdfgen import canvas
 
